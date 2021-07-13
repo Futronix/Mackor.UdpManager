a24e35ac693f7b1bde275ce277b724848d835e60
Gerhard McDonald <gerhard.mcdonald@iotnxt.com>
2021-07-13 15:53:56 +0200

    Add project files.

---
+++ UdpManager/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UDP;
+
+namespace UdpManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UDPSocket s = new UDPSocket();
+            s.Server("127.0.0.1", 9044);
+
+            UDPSocket c = new UDPSocket();
+            c.Client("127.0.0.1", 9044);
+            c.Send("0X800");
+
+            Console.ReadKey();
+        }
+    }
+}
